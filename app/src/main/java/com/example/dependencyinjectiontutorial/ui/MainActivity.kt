1efabeec0a5ba63da87ed2f321702ace70fd7e15
jayraj <jayraj1999@gmail.com>
2021-01-19 17:19:08 +0530

    MVVM - ViewModel , Recycler View , Repository , Dao

--- app/src/main/java/com/example/dependencyinjectiontutorial/ui/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectiontutorial/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.dependencyinjectiontutorial.ui
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
